85ab0efbc7fa0a0e37553dfe840ee14d2c365b4e
GriffinAste-Griphyn <griffin@griphyn.ai>
2026-05-26 22:38:15 -0600

    Make story mention index migration idempotent

--- drizzle/0028_story_mentions_story_id_index.sql
+++ drizzle/0028_story_mentions_story_id_index.sql
@@ -1,1 +1,1 @@
-CREATE INDEX "story_mentions_story_id_idx" ON "story_mentions" USING btree ("story_id", "created_at");
+CREATE INDEX IF NOT EXISTS "story_mentions_story_id_idx" ON "story_mentions" USING btree ("story_id", "created_at");
